c9aab167f68fc5f07cbdb0d86fc7ae01644acea9
NubPaws <DXroomram@gmail.com>
2024-12-10 23:57:59 +0200

    Changed library loading into a single general file called loader.js

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # nubpaws.github.io
 A work in progress website meant to display my projects and other random things I do.
 
-Site can be views on github.io pages at [nubpaws.github.io](https://nubpaws.github.io).
+Site can be viewed on github.io pages at [nubpaws.github.io](https://nubpaws.github.io).
+
+# Resources
+- [Math Jax](https://www.mathjax.org/) for rendering beautiful, crisp, latex code.
+- [highlight.js](https://highlightjs.org/) for pretty syntax coloring in code blocks.
---
+++ loader.js
@@ -0,0 +1,64 @@
+// Helper to load a script dynamically.
+function loadScript(url) {
+	return new Promise((resolve, reject) => {
+		const script = document.createElement("script");
+		script.src = url;
+		script.async = true;
+		script.onload = () => {
+			loadedScripts.add(url);
+			resolve();
+		};
+		script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+		
+		document.head.appendChild(script);
+	});
+}
+
+// Helper to load a stylesheet dynamically.
+function loadStyle(url) {
+	return new Promise((resolve, reject) => {
+		if (loadStyles.has(url)) {
+			resolve();
+			return;
+		}
+		
+		const link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.href = url;
+		link.onload = () => {
+			loadedScripts.add(url);
+			resolve();
+		};
+		link.onerror = () => reject(new Error(`Failed to load stylesheet: ${url}`));
+		
+		document.head.appendChild(link);
+	});
+}
+
+// Load the MathJax script.
+(function () {
+	// Load MathJax
+	window.MathJax = {
+		loader: { load: ["[tex]/color"] },
+		tex: {
+			inlineMath: [["$", "$"]],
+			displayMath: [["$$", "$$"]],
+		},
+		svg: {
+			// Optional but improves performance apparently.
+			fontCache: "global",
+			mtextInheritColor: true,
+		},
+	};
+	loadScript("https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-svg.js");
+	
+	// Load highlight.js
+	Promise.all([
+		loadScript("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"),
+		loadStyle("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css"),
+	]).then(() => {
+		document.querySelectorAll("pre code").forEach(block => { hljs.highlightElement(block); })
+	});
+	
+})();
+
