c5ccc9dbcc4665ea048c694af4ccba63ef705113
Rami Hashan <dxroomram@gmail.com>
2025-06-21 02:08:31 +0300

    fix: base and site missing from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nubpaws.github.io',
+  base: '/',
   integrations: [
     mdx({
       remarkPlugins: [remarkMath],
